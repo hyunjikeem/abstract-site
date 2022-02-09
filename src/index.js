938a932c71c1f4b8ebfed6bebc59192b7cde6241
HyunjiKim818 <kjhxxxx@gmail.com>
2022-02-09 22:35:01 +0900

    test: 기본 테스트 코드 작성

--- src/index.js
+++ src/index.js
@@ -2,39 +2,41 @@ class Site {
     constructor() {
         this.boards = [];
     }
-    
+
     addBoard(board) {
-        if (this.boards.find((boardName) => boardName.name === board.name)) {
-            throw new Error();
-        } else {
+        const existedboard = this.boards.find((boardName) => boardName.name === board.name);
+
+        if (!existedboard) {
             board.plusBoard = true;
             this.boards.push(board);
-        }
+        } else throw new Error();
+        // if (this.boards.find((boardName) => boardName.name === board.name))
+        // throw new Error();
+
+        // board.plusBoard = true;
+        // this.boards.push(board);
     }
 
     findBoardByName(name) {
-        return this.boards.find((board) => board.name = name);
+        return this.boards.find((board) => (board.name = name));
     }
 }
 
 class Board {
     constructor(name) {
-        if (name === null || name === "") {
-            throw new Error();
-        }
+        if (name === null || name === '') throw new Error();
+
         this.name = name;
         this.articles = [];
         this.plusBoard = false;
     }
 
     publish(article) {
-        if (this.plusBoard == false) {
-            throw new Error();
-        } else {
-            article.id = `${this.name}-${Math.random()}`;
-            article.createdDate = new Date().toISOString();
-            this.articles.push(article);
-        }
+        if (!this.plusBoard) throw new Error();
+
+        article.id = `${this.name}-${Math.random()}`;
+        article.createdDate = new Date().toISOString();
+        this.articles.push(article);
     }
 
     getAllArticles() {
@@ -44,8 +46,8 @@ class Board {
 
 class Article {
     constructor(article) {
-        const {subject, content, author} = article;
-        if (subject === null || subject === '' || content === null || content === '' || author === null || author === '') {
+        const { subject, content, author } = article;
+        if (subject === (null || '') || content === (null || '') || author === (null || '')) {
             throw new Error();
         } else {
             this.subject = subject;
@@ -56,12 +58,10 @@ class Article {
     }
 
     reply(comment) {
-        if (!this.id) {
-            throw new Error();
-        } else {
-            comment.createdDate = new Date().toISOString();
-            this.comments.push(comment);
-        }
+        if (!this.id) throw new Error();
+
+        comment.createdDate = new Date().toISOString();
+        this.comments.push(comment);
     }
 
     getAllComments() {
@@ -71,13 +71,12 @@ class Article {
 
 class Comment {
     constructor(comment) {
-        const {content, author} = comment;
-        if (content === null || content === "" || author === null || author === "") {
+        const { content, author } = comment;
+        if (content === (null || "") || author === (null || "")) 
             throw new Error();
-        } else {
-            this.content = content;
-            this.author = author;
-        }
+
+        this.content = content;
+        this.author = author;
     }
 }
 
